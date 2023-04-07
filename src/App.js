1b534263fec460c611c2c207b8e72edd3c6e6d1f
Devansh <dev048673@gmail.com>
2023-04-07 21:23:12 +0530

    Book List

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import View from './View';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello</h1> */}
+      {/* <View /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
